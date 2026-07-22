96b577e5baee7d3cab043dd5c1223bf2cbe55451
Manish Dange <dangemanish35@gmail.com>
2026-07-22 11:29:53 +0530

    Time: 0 ms (100%), Space: 42 MB (77.74%) - LeetHub

---
+++ 1201-ugly-number-iii/1201-ugly-number-iii.java
@@ -0,0 +1,49 @@
+class Solution {
+
+    public int nthUglyNumber(int n, int a, int b, int c) {
+
+        long left = 1;
+        long right = 2_000_000_000L;
+
+        long ab = lcm(a, b);
+        long ac = lcm(a, c);
+        long bc = lcm(b, c);
+        long abc = lcm(ab, c);
+
+        while (left < right) {
+
+            long mid = left + (right - left) / 2;
+
+            long count = mid / a
+                       + mid / b
+                       + mid / c
+                       - mid / ab
+                       - mid / ac
+                       - mid / bc
+                       + mid / abc;
+
+            if (count >= n) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return (int) left;
+    }
+
+    private long gcd(long x, long y) {
+
+        while (y != 0) {
+            long temp = y;
+            y = x % y;
+            x = temp;
+        }
+
+        return x;
+    }
+
+    private long lcm(long x, long y) {
+        return x / gcd(x, y) * y;
+    }
+}
