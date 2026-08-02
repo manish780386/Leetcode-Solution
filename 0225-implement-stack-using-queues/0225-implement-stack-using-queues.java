1c8f8ac14151d77f8d8c6e0c182ee10d5c0bf9a9
Manish Dange <dangemanish35@gmail.com>
2026-08-02 18:05:31 +0530

    Time: 1 ms (74.36%), Space: 42.6 MB (91.91%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class MyStack {
+
+    Queue<Integer> queue;
+
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+
+    public void push(int x) {
+
+        queue.offer(x);
+
+        int size = queue.size();
+
+        while (size > 1) {
+            queue.offer(queue.poll());
+            size--;
+        }
+    }
+
+    public int pop() {
+        return queue.poll();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
